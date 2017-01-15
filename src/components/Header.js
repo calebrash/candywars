f102d362396c55a1327ab990718d1490f91595ac
Caleb Rash <caleb@brilliant.org>
2017-01-15 13:52:12 -0600

    Calculate net capacity

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,14 @@ import { connect } from 'react-redux';
 
 class Header extends React.Component {
   render() {
+    let inventoryTotal = Object.keys(this.props.gameReducer.inventory).reduce((total, drug) => {
+      return total + this.props.gameReducer.inventory[drug];
+    }, 0);
     return (
       <div className="header">
         <span>Day: {this.props.gameReducer.day}</span>
         <span>Cash: {this.props.gameReducer.balance}</span>
-        <span>Capacity: {this.props.gameReducer.capacity}</span>
+        <span>Capacity: {this.props.gameReducer.capacity - inventoryTotal}</span>
       </div>
     );
   }
